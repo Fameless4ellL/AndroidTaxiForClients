6a7b722bb1a5db7f084076ee9ca993be473c2a13
Zaur <fameless.4ell@gmail.com>
2020-08-23 14:36:19 +0200

    Initial commit

---
+++ app/src/main/java/com/fameless/androiduberriderremake/Model/RiderModel.java
@@ -0,0 +1,40 @@
+package com.fameless.androiduberriderremake.Model;
+
+public class RiderModel {
+    private String firstName,lastName,phoneNumber, Avatar;
+
+    public RiderModel() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatar() {
+        return Avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        Avatar = avatar;
+    }
+}
